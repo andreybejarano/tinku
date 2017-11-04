676e0e22b3db9c70365432dce711996eeeccee7e
andreybejarano <andreybejarano@gmail.com>
2017-11-04 00:20:53 -0300

    algo de resposive villero

--- src/containers/MusicListScreen.js
+++ src/containers/MusicListScreen.js
@@ -186,23 +186,23 @@ export default class MusicListScreen extends Component {
 			<View style={[styles.container, {width: window.width, height: window.height}]}>
 				<View style={{ flex: 1, flexDirection: 'row' }}>
 					<TouchableOpacity onPress={() => this.goToMusicGameScreen('cumbia')} style={[styles.touchable, { flex: 1}]}>
-						<Image style={[styles.touchable, { width: '100%', height: '100%' }]} source={require('../statics/images/fondoMenuCumbia.jpg')}>
-							<Image source={require('../statics/images/cumbia.png')} />
+						<Image style={[styles.touchable, { width: window.width, height: window.height }]} source={require('../statics/images/fondoMenuCumbia.jpg')}>
+							<Image style={{width: window.width*0.062, height:window.height*0.805 }} source={require('../statics/images/cumbia.png')} />
 						</Image>
 					</TouchableOpacity>
 					<TouchableOpacity onPress={() => this.goToMusicGameScreen('tango')} style={[styles.touchable, { flex: 1 }]}>
-						<Image style={[styles.touchable, { width: '100%', height: '100%' }]} source={require('../statics/images/fondoMenuTango.jpg')}>
-							<Image source={require('../statics/images/tango.png')} />
+						<Image style={[styles.touchable, { width: window.width, height: window.height }]} source={require('../statics/images/fondoMenuTango.jpg')}>
+							<Image style={{width: window.width*0.062, height:window.height*0.805 }} source={require('../statics/images/tango.png')} />
 						</Image>
 					</TouchableOpacity>
 					<TouchableOpacity onPress={() => this.goToMusicGameScreen('samba')} style={[styles.touchable, { flex: 1 }]}>
-						<Image style={[styles.touchable, { width: '100%', height: '100%' }]} source={require('../statics/images/fondoMenuSamba.jpg')}>
-							<Image source={require('../statics/images/samba.png')} />
+						<Image style={[styles.touchable, { width: window.width, height: window.height }]} source={require('../statics/images/fondoMenuSamba.jpg')}>
+							<Image style={{width: window.width*0.067, height:window.height*0.805 }} source={require('../statics/images/samba.png')} />
 						</Image>
 					</TouchableOpacity>
 					<TouchableOpacity onPress={() => this.goToMusicGameScreen('huaino')} style={[styles.touchable, { flex: 1 }]}>
-						<Image style={[styles.touchable, { width: '100%', height: '100%' }]} source={require('../statics/images/fondoMenuHuaino.jpg')}>
-							<Image source={require('../statics/images/huaino.png')} />
+						<Image style={[styles.touchable, { width: window.width, height: window.height }]} source={require('../statics/images/fondoMenuHuaino.jpg')}>
+							<Image style={{width: window.width*0.057, height:window.height*0.805 }} source={require('../statics/images/huaino.png')} />
 						</Image>
 					</TouchableOpacity>
 				</View>
